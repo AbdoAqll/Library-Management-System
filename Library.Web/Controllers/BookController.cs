e205bdbb2e68d0903c82fa4ac7226a0d307ba478
Kareem Magdy <kareemmagdy1175@gmail.com>
2024-09-28 23:53:31 +0300

    Implement Some Book Controller functionalities and design Details page layout.
    - Implement Book Controller: Get Details & Delete Functionality.
    - Design book details page layout.

--- Library.Web/Controllers/BookController.cs
+++ Library.Web/Controllers/BookController.cs
@@ -14,10 +14,36 @@ namespace Library.Web.Controllers
             this.webHostEnvironment = webHostEnvironment;
         }
 
+<<<<<<< Updated upstream
         public async Task<IActionResult> Index()
+=======
+        public IActionResult Index() 
+>>>>>>> Stashed changes
         {
             var books = await unitOfWork.BookRepository.GetAllAsync();
             return View(books);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var book = unitOfWork.BookRepository.GetFirstOrDefault(x => x.Id == id);
+            return View(book);
+        }
+        public IActionResult Delete(int id)
+        {
+            var book = unitOfWork.BookRepository.GetFirstOrDefault(x => x.Id == id);
+
+            // deleting the book image from wwwroot
+            var oldImagePath = Path.Combine(webHostEnvironment.WebRootPath, book.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+                System.IO.File.Delete(oldImagePath);
+            
+            unitOfWork.BookRepository.Remove(book);
+            unitOfWork.Complete();
+            TempData["Delete"] = "Book deleted successfully";
+
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
