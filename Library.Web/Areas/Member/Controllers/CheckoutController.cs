1af1f5c67aff09a20eb3b1004bb1e48e59b2dd5a
عبدالرحمن نصر عقل خليل <102249752+AbdoAqll@users.noreply.github.com>
2024-10-16 14:44:47 +0300

    Add search bar for Member checkouts

--- Library.Web/Areas/Member/Controllers/CheckoutController.cs
+++ Library.Web/Areas/Member/Controllers/CheckoutController.cs
@@ -2,6 +2,7 @@
 using Library_Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 
 namespace Library.Web.Areas.Member.Controllers
@@ -24,6 +25,27 @@ namespace Library.Web.Areas.Member.Controllers
 
             var checkouts = await unitOfWork.CheckoutRepository.GetAllAsync(x => x.ApplicationUserId == userId && x.Status != StaticData.InCart, "Book");
             return View(checkouts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string? searchString)
+        {
+            if (string.IsNullOrWhiteSpace(searchString) || string.IsNullOrEmpty(searchString))
+            {
+                TempData["Delete"] = "Empty Input!";
+                return RedirectToAction("Index");
+            }
+            var books = await unitOfWork.CheckoutRepository.GetAllAsync(
+                x => x.Book.Title.ToLower().Contains(searchString.ToLower()),"Book");
+
+
+            if (books.IsNullOrEmpty())
+            {
+                TempData["Delete"] = "Not Found!";
+                return RedirectToAction("Index");
+            }
+
+            return View("Index", books);
         }
 
     }
